8a79b0601aac3e78517c4e50368f9f642990d84a
rupareliyagirish <rupareliya.girish@gmail.com>
2015-08-24 21:25:23 -0700

    Sendgrid Task - Done
    Sendgrid Task - Done

---
+++ src/partials/contact-form-sendgrid.php
@@ -0,0 +1,82 @@
+
+<?php
+
+$url = 'https://api.sendgrid.com/';
+$user = 'steamfab';
+$pass = 'iMNpt42NwHYPpu';
+
+$errors = '';
+$myemail = 'rupareliya.girish@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) ||
+   empty($_POST['location'])|| 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message'];
+$location = $_POST['location']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n City/Country:$location \n Message \n $message"; 
+$params = array(
+    'api_user'  => $user,
+    'api_key'   => $pass,
+    'to'        => 'rupareliya.girish@gmail.com',
+    'subject'   => $email_subject,
+    'html'      => '',
+    'text'      => $email_body,
+    'from'      => $email_address
+  );
+
+
+$request =  $url.'api/mail.send.json';
+
+// Generate curl request
+$session = curl_init($request);
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+// Tell PHP not to use SSLv3 (instead opting for TLS)
+curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// obtain response
+$response = curl_exec($session);
+curl_close($session);
+
+// print everything out
+print_r($response);
+}
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
